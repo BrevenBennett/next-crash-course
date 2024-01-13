3fdb4d22df9b20c1f2d0fd5c2eced952dbbf64c3
BrevenBennett <Breven.stars@gmail.com>
2024-01-13 15:56:00 -0600

    learned how to implement server side rendering

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-const Posts = () => {
-  return (
-    <h1>This the Posts Page</h1>
-  )
+async function getData() {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  return data;
 }
 
-export default Posts
+export default async function Posts() {
+  const posts = await getData();
+  return (
+    <>
+      <h1>This the Posts Page</h1>
+      {posts?.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </>
+  );
+}
